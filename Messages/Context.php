21a23b483dc291f1e3b5c32e76dac70f9d248a28
Luciano Mammino <Luciano.Mammino@smartbox.com>
2016-01-18 17:38:58 +0000

    unnamed commit

--- Messages/Context.php
+++ Messages/Context.php
@@ -9,7 +9,7 @@ use Smartbox\CoreBundle\Type\SerializableArray;
  * Class Context
  * @package Smartbox\Integration\FrameworkBundle\Messages
  */
-class Context
+class Context implements \ArrayAccess
 {
     const TRANSACTION_ID = 'transaction_id';
     const ORIGINAL_FROM = 'from';
@@ -48,5 +48,37 @@ class Context
     public function get($key)
     {
         return $this->values->get($key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->values[$offset]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function offsetGet($offset)
+    {
+        return @$this->values[$offset];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->values[$offset] = $value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->values[$offset]);
     }
 }
