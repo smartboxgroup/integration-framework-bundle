f0102600ac97cfebd94bb6c832174fe8b4a4191f
nacho <ignacio.galarza@smartbox.com>
2016-02-12 12:39:14 +0000

    Unit test refactored using smartesbHelper. Unit test fixed.

--- Tests/Unit/Events/EventDispatcherTest.php
+++ Tests/Unit/Events/EventDispatcherTest.php
@@ -8,6 +8,7 @@ use Smartbox\Integration\FrameworkBundle\Drivers\Queue\ArrayQueueDriver;
 use Smartbox\Integration\FrameworkBundle\Events\EventDispatcher;
 use Smartbox\Integration\FrameworkBundle\Events\EventFilterInterface;
 use Smartbox\Integration\FrameworkBundle\Events\EventFiltersRegistry;
+use Smartbox\Integration\FrameworkBundle\Helper\SmartesbHelper;
 use Smartbox\Integration\FrameworkBundle\Messages\EventMessage;
 use Smartbox\Integration\FrameworkBundle\Messages\MessageFactory;
 use Smartbox\Integration\FrameworkBundle\Messages\Queues\QueueMessage;
@@ -29,13 +30,16 @@ class EventDispatcherTest extends \PHPUnit_Framework_TestCase{
         $queueDriver->setMessageFactory($messageFactory);
 
         $event = new HandlerEvent();
+        $helper = new SmartesbHelper();
 
         $container = new Container();
         $container->set('smartesb.registry.event_filters',$filtersRegistry);
         $container->set('smartesb.drivers.queue.events',$queueDriver);
+        $container->set('smartesb.helper',$helper);
         $container->setParameter('smartesb.flows_version', '0');
 
         $container->setParameter('smartesb.events_queue_name', 'test_queue');
+        $helper->setContainer($container);
 
         $dispatcher = new EventDispatcher($container);
         $dispatcher->dispatch("test_event", $event);
