01d2486a756c300b8472f5740fa559f96819918f
Jose Rufino <jose.rufino@smartbox.com>
2016-03-04 15:11:30 +0000

    Review changes

--- SmartboxIntegrationFrameworkBundle.php
+++ SmartboxIntegrationFrameworkBundle.php
@@ -4,6 +4,7 @@ namespace Smartbox\Integration\FrameworkBundle;
 
 use Smartbox\Integration\FrameworkBundle\DependencyInjection\ConnectorsCompilerPass;
 use Smartbox\Integration\FrameworkBundle\DependencyInjection\EventDeferringCompilerPass;
+use Smartbox\Integration\FrameworkBundle\DependencyInjection\MockWebserviceClientsCompilerPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -15,5 +16,9 @@ class SmartboxIntegrationFrameworkBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new EventDeferringCompilerPass(),PassConfig::TYPE_AFTER_REMOVING);
+
+        if($container->getParameter('kernel.environment') == 'test'){
+            $container->addCompilerPass(new MockWebserviceClientsCompilerPass(),PassConfig::TYPE_AFTER_REMOVING);
+        }
     }
 }
