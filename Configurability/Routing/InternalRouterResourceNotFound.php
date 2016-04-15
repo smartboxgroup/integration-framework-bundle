6b031006b3fcbe76a84d27b8cbf5c1545d21cc46
David Camprubi <david.camprubi@smartbox.com>
2016-04-15 14:55:30 +0000

    Create InternalRouterResourceNotFound to throw 500 errors for internal resources not found

---
+++ Configurability/Routing/InternalRouterResourceNotFound.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Smartbox\Integration\FrameworkBundle\Configurability\Routing;
+
+use Symfony\Component\Routing\Exception\ExceptionInterface;
+
+class InternalRouterResourceNotFound extends \RuntimeException implements ExceptionInterface
+{
+}
