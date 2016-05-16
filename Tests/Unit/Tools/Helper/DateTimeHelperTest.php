86dbb0657550756b4a20debdcf138ab1a9bc7166
Jose Rufino <jose.rufino@smartbox.com>
2016-05-16 12:33:56 +0100

    Fix problem related with DateTime::createFromFormat

---
+++ Tests/Unit/Tools/Helper/DateTimeHelperTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Smartbox\Integration\FrameworkBundle\Tests\Unit\Messages;
+
+use Smartbox\CoreBundle\Type\SerializableArray;
+use Smartbox\CoreBundle\Type\String;
+use Smartbox\Integration\FrameworkBundle\Core\Messages\Context;
+use Smartbox\Integration\FrameworkBundle\Tools\Helper\DateTimeHelper;
+
+/**
+ * Class ContextTest.
+ */
+class ContextTest extends \PHPUnit_Framework_TestCase
+{
+    public function timestampCaseProvider(){
+        return [
+            ['1463132313.1234'],
+            ['1463132313.0000'],
+            [1463132313.1234],
+            [1463132313.0000],
+            [1463132313]
+        ];
+    }
+
+    /**
+     * @dataProvider timestampCaseProvider
+     */
+    public function testCreateDateTimeFromTimestampWithMilliseconds($millis)
+    {
+        $datetime = DateTimeHelper::createDateTimeFromTimestampWithMilliseconds($millis);
+        $this->assertInstanceOf(\DateTime::class,$datetime);
+    }
+
+    public function testCreateDateTimeFromCurrentMicrotime(){
+        $datetime = DateTimeHelper::createDateTimeFromCurrentMicrotime();
+        $this->assertInstanceOf(\DateTime::class,$datetime);
+    }
+}
