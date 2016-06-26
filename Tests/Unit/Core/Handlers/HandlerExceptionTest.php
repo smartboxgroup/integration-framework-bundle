022779ba06b9258d5aff1ec872fe6a20267a40c4
David Camprubi <david.camprubi@smartbox.com>
2016-06-26 21:30:37 +0000

    Create HandlerExceptionTest

---
+++ Tests/Unit/Core/Handlers/HandlerExceptionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Smartbox\Integration\FrameworkBundle\Tests\Unit\Core\Handlers;
+
+use Smartbox\Integration\FrameworkBundle\Core\Handlers\HandlerException;
+use Smartbox\Integration\FrameworkBundle\Core\Messages\MessageInterface;
+
+class HandlerExceptionTests extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        /** @var $messageInterface $messageInterface */
+        $messageInterface = $this->createMock(MessageInterface::class);
+        $message          = 'Handler exception';
+
+        $handlerException = new HandlerException($message, $messageInterface);
+
+        $this->assertSame($message, $handlerException->getMessage());
+        $this->assertSame($messageInterface, $handlerException->getFailedMessage());
+    }
+
+    public function testSetAndGetFailedMessage()
+    {
+        /** @var $messageInterface $messageInterface */
+        $messageInterface = $this->createMock(MessageInterface::class);
+        $failedMessage = $this->createMock(MessageInterface::class);
+
+        $handlerException = new HandlerException('', $messageInterface);
+        $handlerException->setFailedMessage($failedMessage);
+
+        $this->assertSame($failedMessage, $handlerException->getFailedMessage());
+    }
+}
