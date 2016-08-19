30d5fb7f425f9a0033c1e3290aa64c204fc746cd
David Camprubi <david.camprubi@smartbox.com>
2016-08-19 10:21:25 +0000

    Rename all the methods related to itinerary class because were changed in another task

--- Core/Handlers/BasicMessageHandler.php
+++ Core/Handlers/BasicMessageHandler.php
@@ -6,11 +6,8 @@ use Smartbox\Integration\FrameworkBundle\Configurability\Routing\InternalRouter;
 use Smartbox\Integration\FrameworkBundle\Core\Endpoints\EndpointInterface;
 use Smartbox\Integration\FrameworkBundle\Core\Exchange;
 use Smartbox\Integration\FrameworkBundle\Core\Messages\Context;
-use Smartbox\Integration\FrameworkBundle\Core\Messages\DeferredExchangeEnvelope;
-use Smartbox\Integration\FrameworkBundle\Core\Messages\ErrorExchangeEnvelope;
 use Smartbox\Integration\FrameworkBundle\Core\Messages\ExchangeEnvelope;
 use Smartbox\Integration\FrameworkBundle\Core\Messages\FailedExchangeEnvelope;
-use Smartbox\Integration\FrameworkBundle\Core\Messages\Message;
 use Smartbox\Integration\FrameworkBundle\Core\Messages\MessageInterface;
 use Smartbox\Integration\FrameworkBundle\Core\Messages\RetryExchangeEnvelope;
 use Smartbox\Integration\FrameworkBundle\Core\Processors\Exceptions\ProcessingException;
@@ -18,18 +15,20 @@ use Smartbox\Integration\FrameworkBundle\Core\Processors\Processor;
 use Smartbox\Integration\FrameworkBundle\Core\Processors\ProcessorInterface;
 use Smartbox\Integration\FrameworkBundle\DependencyInjection\Traits\UsesItineraryResolver;
 use Smartbox\Integration\FrameworkBundle\Events\HandlerEvent;
-use Smartbox\Integration\FrameworkBundle\Events\NewExchangeEvent;
 use Smartbox\Integration\FrameworkBundle\Events\ProcessingErrorEvent;
 use Smartbox\Integration\FrameworkBundle\Exceptions\RecoverableExceptionInterface;
 use Smartbox\Integration\FrameworkBundle\Service;
 use Smartbox\Integration\FrameworkBundle\DependencyInjection\Traits\UsesEndpointFactory;
 use Smartbox\Integration\FrameworkBundle\DependencyInjection\Traits\UsesEventDispatcher;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 /**
  * Class MessageHandler.
  */
-class BasicMessageHandler extends Service implements HandlerInterface
+class BasicMessageHandler extends Service implements HandlerInterface, ContainerAwareInterface
 {
+    use ContainerAwareTrait;
     use UsesEventDispatcher;
     use UsesEndpointFactory;
     use UsesItineraryResolver;
@@ -171,14 +170,25 @@ class BasicMessageHandler extends Service implements HandlerInterface
     {
         $itinerary = $exchange->getItinerary();
 
-        if (!$itinerary || empty($itinerary->getProcessors())) {
+        if (!$itinerary || empty($itinerary->getProcessorIds())) {
             throw new HandlerException('Itinerary not found while handling message', $exchange->getIn());
         }
 
         $progress = 0;
         $exchangeBackup = clone $exchange;
 
-        while (null !== $processor = $itinerary->shiftProcessor()) {
+        while (null !== $processorId = $itinerary->shiftProcessorId()) {
+            // Get the processor from the container
+            if (!$this->container->has($processorId)) {
+                throw new \RuntimeException("Processor with id $processorId not found.");
+            }
+
+            $processor = $this->container->get($processorId);
+            if (!$processor instanceof ProcessorInterface) {
+                throw new \RuntimeException("Processor with id $processorId does not implement ProcessorInterface.");
+            }
+
+            // Execute the processor
             try {
                 $this->prepareExchange($exchange);
 
