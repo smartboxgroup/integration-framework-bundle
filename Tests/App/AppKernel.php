9f1940674f10849d467cd788af9f38be36d01301
David Camprubi <david.camprubi@smartbox.com>
2016-08-24 11:16:04 +0000

    Remove doctrine as a dependency in framework bundle

--- Tests/App/AppKernel.php
+++ Tests/App/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new \JMS\SerializerBundle\JMSSerializerBundle(),
             new \BeSimple\SoapBundle\BeSimpleSoapBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
 
             new \Smartbox\CoreBundle\SmartboxCoreBundle(),
             new \Smartbox\Integration\FrameworkBundle\SmartboxIntegrationFrameworkBundle(),
