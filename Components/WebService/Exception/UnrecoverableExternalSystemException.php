a96ea394dcc963def5568d52d05f407a1b996670
mel.mccann <mel.mccann@smartbox.com>
2017-06-22 10:54:55 +0000

    display_message in validation step

---
+++ Components/WebService/Exception/UnrecoverableExternalSystemException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Smartbox\Integration\FrameworkBundle\Components\WebService\Exception;
+
+use Smartbox\Integration\FrameworkBundle\Exceptions\UnrecoverableExceptionInterface;
+
+/**
+ * Class UnrecoverableExternalSystemException.
+ */
+class UnrecoverableExternalSystemException extends ExternalSystemException implements UnrecoverableExceptionInterface
+{
+}
