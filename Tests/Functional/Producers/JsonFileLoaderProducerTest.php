f9fc7df7aef8a5408515775af5f874d672f5b185
Arthur Thevenet <arthur.thevenet@smartbox.com>
2017-08-23 15:34:23 +0000

    Add test for the new header feature

--- Tests/Functional/Producers/JsonFileLoaderProducerTest.php
+++ Tests/Functional/Producers/JsonFileLoaderProducerTest.php
@@ -17,7 +17,7 @@ class JsonFileLoaderProducerTest extends BaseTestCase
     public function setUp()
     {
         parent::setUp();
-        $this->producer = new \Smartbox\Integration\FrameworkBundle\Components\JsonFileLoader\JsonFileLoaderProducer();
+        $this->producer = new JsonFileLoaderProducer();
         $this->producer->setSerializer(self::getContainer()->get('serializer'));
     }
 
@@ -29,6 +29,7 @@ class JsonFileLoaderProducerTest extends BaseTestCase
         $opts = [
             JsonFileLoaderProtocol::OPTION_BASE_PATH => $pathFixtures,
             JsonFileLoaderProtocol::OPTION_FILENAME => 'entity_x.json',
+            JsonFileLoaderProtocol::OPTION_TYPE => JsonFileLoaderProtocol::OPTION_TYPE_VALUE_BODY
         ];
 
         $this->producer->send($exchange, new Endpoint('xxx', $opts, new JsonFileLoaderProtocol()));
@@ -38,6 +39,28 @@ class JsonFileLoaderProducerTest extends BaseTestCase
         $sample->setX(100);
 
         $content = $exchange->getResult()->getBody();
+        $this->assertEquals($sample, $content);
+    }
+
+    public function testSendShouldWorkForHeader()
+    {
+        $pathFixtures = realpath(__DIR__.'/../../Fixtures');
+        $exchange = new Exchange($this->createMessage(new EntityX(1)));
+
+        $opts = [
+            JsonFileLoaderProtocol::OPTION_BASE_PATH => $pathFixtures,
+            JsonFileLoaderProtocol::OPTION_FILENAME => 'headers.json',
+            JsonFileLoaderProtocol::OPTION_TYPE => JsonFileLoaderProtocol::OPTION_TYPE_VALUE_HEADERS
+        ];
+
+        $this->producer->send($exchange, new Endpoint('xxx', $opts, new JsonFileLoaderProtocol()));
+
+        $sample = [
+            "header1" => "test",
+            "header2" => "test2"
+        ];
+
+        $content = $exchange->getResult()->getHeaders();
         $this->assertEquals($sample, $content);
     }
 }
