cdea5393144cf21230dee46e7e9ef966c2978d3b
mel.mccann <mel.mccann@smartbox.com>
2017-10-19 17:47:33 +0200

    Assign more memory and also deal with travisci apcu problem.

---
+++ travis/install-apcu-pecl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+php --version
+php --ini
+
+if [[ "$(php -v | grep 'PHP 7.0')" ]] ; then
+    php --ini
+    yes | pecl -f install channel://pecl.php.net/APCu-5.1.8;
+
+elif [[ "$(php -v | grep 'PHP 7.1')" ]] ; then
+    yes | pecl -f install channel://pecl.php.net/APCu-5.1.8;
+else
+    yes | pecl -f install channel://pecl.php.net/APCu-4.0.7;
+fi
+
+php -m | grep apc
