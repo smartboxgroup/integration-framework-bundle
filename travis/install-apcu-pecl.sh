ab4cfb8e973016b424fe29441cc9dd379779c556
David Camprubi <david.camprubi@smartbox.com>
2017-10-23 21:38:44 +0000

    Simplify php.ini configuration and apcu installation

--- travis/install-apcu-pecl.sh
+++ travis/install-apcu-pecl.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-if [[ "$(php -v | grep 'PHP 7.0')" ]] ; then
-    yes | pecl -f install channel://pecl.php.net/APCu-5.1.8;
-elif [[ "$(php -v | grep 'PHP 7.1')" ]] ; then
+if [[ $TRAVIS_PHP_VERSION = 7.* ]]; then
     yes | pecl -f install channel://pecl.php.net/APCu-5.1.8;
 else
-    yes | pecl -f install channel://pecl.php.net/APCu-4.0.7;
+    yes | pecl -f install channel://pecl.php.net/APCu-4.0.11;
 fi
 
 php -m | grep apc
