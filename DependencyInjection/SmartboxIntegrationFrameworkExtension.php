23a939fe99a5efe7418fd72b4f575cdb2a4db651
Shane McKinley <moonraking@users.noreply.github.com>
2017-11-22 13:32:35 +0000

    To mitigate breaking changes, default the new throttle_uri to retry_uri

--- DependencyInjection/SmartboxIntegrationFrameworkExtension.php
+++ DependencyInjection/SmartboxIntegrationFrameworkExtension.php
@@ -308,7 +308,15 @@ class SmartboxIntegrationFrameworkExtension extends Extension
             $handlerDef->addMethodCall('setThrottleDelay', [$handlerConfig['throttle_delay']]);
             $handlerDef->addMethodCall('setThrottleStrategy', [$handlerConfig['throttle_strategy']]);
             $handlerDef->addMethodCall('setThrottleDelayFactor', [$handlerConfig['throttle_delay_factor']]);
-            $handlerDef->addMethodCall('setThrottleURI', [$handlerConfig['throttle_uri']]);
+
+            // If there is no throttle_uri set, we need to set it with something, so we will use the retry uri.
+            // This will maintains the current behaviour, but allows us to explicitly set a throttle_uri
+            if(isset($handlerConfig['throttle_uri'])){
+                $handlerDef->addMethodCall('setThrottleURI', [$handlerConfig['throttle_uri']]);
+            }
+            else{
+                $handlerDef->addMethodCall('setThrottleURI', [$handlerConfig['retry_uri']]);
+            }
 
             $handlerDef->addMethodCall('setThrowExceptions', [$handlerConfig['throw_exceptions']]);
             $handlerDef->addMethodCall('setDeferNewExchanges', [$handlerConfig['defer_new_exchanges']]);
