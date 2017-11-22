9c94bd4630175283012d3d5b2f13ff6167b43114
Bertrand Drouhard <bertrand@drouhard.com>
2017-11-22 14:40:20 +0000

    Bugfix/install apcu configuration (#25)
    * Re-commit as first pull request did not go so well
    
    * Add throttle_X to the handler definitions and tidy the order of the defnitions, change the deferRetryExhangeMessage method to defer to different endpoints, change how the queuemessage is build depending on retry or throttle
    
    * Refactor so that retry and throttle endpoints are lazy loaded and not created when just setting their uri, make deferRetryExchangeMessage BC
    
    * Try to force a commit
    
    * Fixes in Mapper related to time functions
    
    * Fixed convertDate example
    
    * Fix spelling mistake and coding standards
    
    * Fixed warning message when installing APCU for Travis
    
    * Fixed warning message when installing APCU for Travis

--- travis/install-apcu-pecl.sh
+++ travis/install-apcu-pecl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PHP_VERSION = 7.* ]]; then
-    yes | pecl -f install channel://pecl.php.net/APCu-5.1.8;
+    yes | pecl install channel://pecl.php.net/APCu-5.1.8;
 else
-    yes | pecl -f install channel://pecl.php.net/APCu-4.0.11;
+    yes | pecl install channel://pecl.php.net/APCu-4.0.11;
 fi
 
 php -m | grep apc
