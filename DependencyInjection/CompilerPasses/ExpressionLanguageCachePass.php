32d8200801f18373c0f99c4bd5f17f1bdf79cec2
Gary PEGEOT <gary.pegeot@smartbox.com>
2019-02-13 15:42:32 +0000

    Fix type error on old Symfony version

--- DependencyInjection/CompilerPasses/ExpressionLanguageCachePass.php
+++ DependencyInjection/CompilerPasses/ExpressionLanguageCachePass.php
@@ -7,6 +7,7 @@ namespace Smartbox\Integration\FrameworkBundle\DependencyInjection\CompilerPasse
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\Kernel;
 
 class ExpressionLanguageCachePass implements CompilerPassInterface
 {
@@ -21,6 +22,11 @@ class ExpressionLanguageCachePass implements CompilerPassInterface
             if (!$container->has($id)) {
                 return;
             }
+        }
+
+        // Only supported starting from Symfony 3.2
+        if (version_compare(Kernel::VERSION, '3.2.0', '<')) {
+            return;
         }
 
         $container->findDefinition('smartesb.util.expression_language')->addArgument(new Reference('cache.app'));
