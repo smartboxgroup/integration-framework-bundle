aada84b42a2a0a88b21f76590874875499639f76
Andres Rey <andres.rey@smartbox.com>
2020-04-01 12:28:48 +0100

    AMQP: Fix service name + remove test that it's impossible to run now

--- Tests/Functional/Drivers/Queue/StompQueueDriverTest.php
+++ Tests/Functional/Drivers/Queue/StompQueueDriverTest.php
@@ -39,42 +39,7 @@ class StompQueueDriverTest extends AbstractQueueDriverTest
      */
     protected function createDriver(): QueueDriverInterface
     {
-        return $this->getContainer()->get('smartesb.drivers.queue.main');
-    }
-
-    /**
-     * Test that in case we catch an exception during send(), the connection gets dropped to avoid interfering
-     * with the following messages.
-     *
-     * @runInSeparateProcess
-     */
-    public function testConnectionGetsDroppedOnException()
-    {
-        $this->expectException(ConnectionException::class);
-        $this->overrideFwrite();
-
-        $msg = $this->createQueueMessage($this->createSimpleEntity());
-        $this->driver->send($msg);
-
-        $this->assertFalse($this->driver->isConnected(), 'Driver still connected after exception.');
-    }
-
-    /*
-     * Override native fwrite function. Test should be run in separate process (@runInSeparateProcess) to avoid
-     * overriding it for every other test.
-     */
-    protected function overrideFwrite()
-    {
-        $function = '
-                namespace Stomp\Network;
-
-                function fwrite()
-                {
-                    return 0;
-                }
-            ';
-
-        eval($function);
+        return $this->getContainer()->get('smartesb.drivers.queue.rabbitmq');
     }
 
     /**
