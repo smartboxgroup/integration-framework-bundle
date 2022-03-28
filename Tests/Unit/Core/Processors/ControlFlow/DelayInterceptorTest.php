fa45fe2783903da783e0f91697af9e7827afa6c9
Jason Machado Silva <jason.silva@smartbox.com>
2022-03-28 20:52:39 +0100

    Add DelayInterceptorTest.php

---
+++ Tests/Unit/Core/Processors/ControlFlow/DelayInterceptorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Smartbox\Integration\FrameworkBundle\Tests\Unit\Core\Processors\ControlFlow;
+
+use Smartbox\CoreBundle\Tests\Fixtures\Entity\TestEntity;
+use Smartbox\Integration\FrameworkBundle\Core\Exchange;
+use Smartbox\Integration\FrameworkBundle\Core\Messages\Message;
+use Smartbox\Integration\FrameworkBundle\Core\Processors\ControlFlow\DelayInterceptor;
+use Smartbox\Integration\FrameworkBundle\Core\Processors\Exceptions\DelayException;
+use Smartbox\Integration\FrameworkBundle\Core\Processors\Exceptions\ProcessingException;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+/**
+ * Class DelayInterceptorTest.
+ *
+ * @coversDefaultClass \Smartbox\Integration\FrameworkBundle\Core\Processors\ControlFlow\DelayInterceptorTest
+ */
+class DelayInterceptorTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test that when we process a message that shall not pass and the processor is asyncDelayed that we do throw an
+     * Exception and that exception has a delay set.
+     */
+    public function testExceptionsDelayIsSet()
+    {
+        $throttlerMock = $this->getMockBuilder(DelayInterceptor::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $eventDispatcherMock = $this->createMock(EventDispatcher::class);
+        $throttlerMock->setEventDispatcher($eventDispatcherMock);
+
+        $exchange = new Exchange(new Message(new TestEntity()));
+
+        //We do not use expectException, instead we want to actually inspect what is in the exception
+        $thrown = false;
+        try {
+            $throttlerMock->process($exchange);
+        } catch (\Exception $e) {
+            $thrown = true;
+            $this->assertInstanceOf(ProcessingException::class, $e);
+            $this->assertInstanceOf(DelayException::class, $e->getOriginalException());
+        }
+
+        $this->assertTrue($thrown, 'Process did not throw an exception');
+    }
+}
